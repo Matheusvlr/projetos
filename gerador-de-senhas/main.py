26fe2b56b62ce8b0ab259bf55d2f539e6615dbff
Matheusvlr <145072053+Matheusvlr@users.noreply.github.com>
2023-11-01 19:31:26 -0300

    Update main.py

--- gerador-de-senhas/main.py
+++ gerador-de-senhas/main.py
@@ -1,13 +1,14 @@
 from tkinter import *
 from tkinter import ttk
-
 from PIL import ImageTk, Image
+import string
+import random
 
 # cores
 cor1 = "#0a0a0a"  # preta
 cor2 = "#fafcff"  # branca
 cor3 = "#21c25c"  # verde
-cor4 = "#eb463b"  # vermelha
+cor4 = "#f50505"  # vermelha
 cor5 = "#dedcdc"  # cinza
 cor6 = "#3080f0"  # azul
 
@@ -16,14 +17,18 @@ janela.title('')
 janela.geometry('295x350')
 janela.configure(bg=cor2)
 
+
+estilo = ttk.Style(janela)
+estilo.theme_use('clam')
+
 # divisão da tela em dois frames
 frame_cima = Frame(janela, width=295, height=50, bg=cor2, pady=0, padx=0, relief='flat')
 frame_cima.grid(row=0, column=0, sticky=NSEW)
 
-frame_baixo = Frame(janela, width=295, height=310, bg=cor1, pady=0, padx=0, relief='flat')
+frame_baixo = Frame(janela, width=295, height=310, bg=cor2, pady=0, padx=0, relief='flat')
 frame_baixo.grid(row=1, column=0, sticky=NSEW)
 
-# Frame cima
+# frame cima
 img = Image.open('C:\\Users\\Matheus\\Documents\\projetos\\gerador-de-senhas\\img\\gerador.png')
 img = img.resize((55, 55), Image.BICUBIC)
 img = ImageTk.PhotoImage(img)
@@ -34,11 +39,65 @@ app_logo.place(x=2, y=0)
 app_nome = Label(frame_cima, text='GERADOR DE SENHAS',width=20, height=1, padx=0, relief='flat', anchor='nw', font=('Ivy 13 bold'),bg=cor2, fg=cor1)
 app_nome.place(x=45, y=13)
 
-app_linha = Label(frame_cima, text='',width=295, height=1, padx=0, relief='flat', anchor='nw', font=('Ivy 1 bold'),bg=cor2, fg=cor1)
-app_linha.place(x=0, y=35)
+app_linha = Label(frame_cima, text='',width=295, height=1, padx=0, relief='flat', anchor='nw', font=('Ivy 1 bold'),bg=cor4, fg=cor1)
+app_linha.place(x=0, y=45)
 
+# frame de baixo
 
-estilo = ttk.Style(janela)
-estilo.theme_use('clam')
+app_senha = Label(frame_baixo, text='- - -', width=26, height=2, padx=0, relief='solid', anchor='center', font=('Ivy 10 bold'),bg=cor2, fg=cor1)
+app_senha.grid(row=0, column=0, columnspan=1, sticky=NSEW, padx=3, pady=10)
+
+app_info = Label(frame_baixo, text='Número de caracteres na senha', height=1, padx=0, relief='flat', anchor='nw', font=('Ivy 10 bold'),bg=cor2, fg=cor1)
+app_info.grid(row=1, column=0, columnspan=2, sticky=NSEW, padx=5, pady=1)
+
+var = IntVar()
+var.set(8)
+spin = Spinbox(frame_baixo, from_= 0, to=20, width=5, textvariable=var)
+spin.grid(row=2, column=0, columnspan=2, sticky=NW, padx=5, pady=8)
+
+alfabeto_maior = string.ascii_uppercase
+alfabeto_menor = string.ascii_lowercase
+numeros = '123456789'
+simbolos = ':@#*{}[]()_/,-'
+
+frame_caracteres = Frame(frame_baixo, width=295, height=210, bg=cor2, pady=0, padx=0, relief='flat')
+frame_caracteres.grid(row=3, column=0, sticky=NSEW)
+
+# letras maiúsculas
+estado_1 = StringVar()
+estado_1.set(False)
+check_1 = Checkbutton(frame_caracteres, width=1, var=estado_1, onvalue=alfabeto_maior, offvalue='off', offrelief='flat', bg=cor2)
+check_1.grid(row=0, column=0, sticky=NSEW, padx=2, pady=5)
+app_info = Label(frame_caracteres, text='Letras maiúsculas (ABC)', height=1, padx=0, relief='flat', anchor='nw', font=('Ivy 10 bold'),bg=cor2, fg=cor1)
+app_info.grid(row=0, column=1, sticky=NW, padx=2, pady=5)
+
+# letras minúsculas
+estado_2 = StringVar()
+estado_2.set(False)
+check_2 = Checkbutton(frame_caracteres, width=1, var=estado_2, onvalue=alfabeto_menor, offvalue='off', offrelief='flat', bg=cor2)
+check_2.grid(row=1, column=0, sticky=NSEW, padx=2, pady=5)
+app_info = Label(frame_caracteres, text='Letras minúsculas (abc)', height=1, padx=0, relief='flat', anchor='nw', font=('Ivy 10 bold'),bg=cor2, fg=cor1)
+app_info.grid(row=1, column=1, sticky=NW, padx=2, pady=5)
+
+# números
+estado_3 = StringVar()
+estado_3.set(False)
+check_3 = Checkbutton(frame_caracteres, width=1, var=estado_3, onvalue=numeros, offvalue='off', offrelief='flat', bg=cor2)
+check_3.grid(row=2, column=0, sticky=NSEW, padx=2, pady=5)
+app_info = Label(frame_caracteres, text='Números (123)', height=1, padx=0, relief='flat', anchor='nw', font=('Ivy 10 bold'),bg=cor2, fg=cor1)
+app_info.grid(row=2, column=1, sticky=NW, padx=2, pady=5)
+
+# símbolos
+estado_4 = StringVar()
+estado_4.set(False)
+check_4 = Checkbutton(frame_caracteres, width=1, var=estado_4, onvalue=simbolos, offvalue='off', offrelief='flat', bg=cor2)
+check_4.grid(row=3, column=0, sticky=NSEW, padx=2, pady=5)
+app_info = Label(frame_caracteres, text='Símbolos (#@*)', height=1, padx=0, relief='flat', anchor='nw', font=('Ivy 10 bold'),bg=cor2, fg=cor1)
+app_info.grid(row=3, column=1, sticky=NW, padx=2, pady=5)
+
+# botão
+botao_gerador_senha = Button(frame_caracteres, text='Gerar Senha',width=34, height=1, relief='flat',overrelief='solid', anchor='center', font=('Ivy 10 bold'),bg=cor4, fg=cor2)
+botao_gerador_senha.grid(row=5, column=0, sticky=NSEW, padx=5, pady=11, columnspan=5)
+
 
 janela.mainloop()
