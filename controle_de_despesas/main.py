00ce75626ac6bc9ea507354515a482b69b5a7eef
Matheusvlr <145072053+Matheusvlr@users.noreply.github.com>
2023-11-06 15:33:27 -0300

    commit

--- controle_de_despesas/main.py
+++ controle_de_despesas/main.py
@@ -1,1 +1,28 @@
 from tkinter import *
+from tkinter import Tk, ttk
+
+################# cores ###############
+cor0 = "#2e2d2b"  
+cor1 = "#feffff"  
+cor2 = "#4fa882"  
+cor3 = "#38576b"  
+cor4 = "#403d3d"   
+cor5 = "#e06636"   
+cor6 = "#038cfc"   
+cor7 = "#3fbfb9"   
+cor8 = "#263238"   
+cor9 = "#e9edf5"   
+
+colors = ['#5588bb', '#66bbbb','#99bb55', '#ee9944', '#444466', '#bb5555']
+
+# criação da janela
+janela = Tk()
+janela.title()
+janela.geometry('900x650')
+janela.configure(background=cor9)
+janela.resizable(width=FALSE, height=FALSE)
+
+style= ttk.Style(janela)
+style.theme_use('clam')
+
+janela.mainloop 
