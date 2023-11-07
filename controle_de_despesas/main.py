08c6c7975c79dad49b1e36477c906baf4e91345f
Matheusvlr <145072053+Matheusvlr@users.noreply.github.com>
2023-11-07 15:33:15 -0300

    update

--- controle_de_despesas/main.py
+++ controle_de_despesas/main.py
@@ -75,6 +75,112 @@ def porcentagem():
     lab_porcentagem = Label(frame_meio, text=f'{valor:,.2f}%', height=1, anchor=NW, font=('verdana 12'), bg=cor1, fg=cor4)
     lab_porcentagem.place(x=200, y=35)
 
+# função para gráfico de barra
+def grafico_barra():
+    lista_categorias =  ['Renda', 'Despesas', 'Saldo']
+    lista_valores = [3000, 2000, 6236]
+
+    # fazer figura e atribua objetos de eixo
+    figura = plt.Figure(figsize=(4, 3.45), dpi=60)
+    ax = figura.add_subplot(111)
+    # ax.autoscale(enable=True, axis='both', tight=None)
+
+    ax.bar(lista_categorias, lista_valores,  color=colors, width=0.9)
+    # create a list to collect the plt.patches data
+
+    c = 0
+    # set individual bar lables using above list
+    for i in ax.patches:
+        # get_x pulls left or right; get_height pushes up or down
+        ax.text(i.get_x()-.001, i.get_height()+.5,
+                str("{:,.0f}".format(lista_valores[c])), fontsize=17, fontstyle='italic',  verticalalignment='bottom',color='dimgrey')
+        c += 1
+
+    ax.set_xticklabels(lista_categorias,fontsize=16)
+
+    ax.patch.set_facecolor('#ffffff')
+    ax.spines['bottom'].set_color('#CCCCCC')
+    ax.spines['bottom'].set_linewidth(1)
+    ax.spines['right'].set_linewidth(0)
+    ax.spines['top'].set_linewidth(0)
+    ax.spines['left'].set_color('#CCCCCC')
+    ax.spines['left'].set_linewidth(1)
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.tick_params(bottom=False, left=False)
+    ax.set_axisbelow(True)
+    ax.yaxis.grid(False, color='#EEEEEE')
+    ax.xaxis.grid(False)
+
+    canva = FigureCanvasTkAgg(figura, frame_meio)
+    canva.get_tk_widget().place(x=10, y=70)
+
+
+# função de resumo total
+def resumo():
+    valor = [500, 600, 420]
+
+    l_linha = Label(frame_meio, text='', width=220, height=1, anchor=NW, font=('Arial 1'), bg='#545454')
+    l_linha.place(x=309, y=52)
+    l_sumario = Label(frame_meio, text='Total da Renda Mensal      '.upper(), anchor=NW, font=('Verdana 11'), bg=cor1, fg='#83a9e6')
+    l_sumario.place(x=309, y=35)
+    l_sumario = Label(frame_meio, text=f'R${valor[0]:,.2f}', anchor=NW, font=('Arial 17'), bg=cor1, fg='#545454')
+    l_sumario.place(x=309, y=70)
+
+    l_linha = Label(frame_meio, text='', width=225, height=1, anchor=NW, font=('Arial 1'), bg='#545454')
+    l_linha.place(x=309, y=132)
+    l_sumario = Label(frame_meio, text='Total de Despesas Mensais '.upper(), anchor=NW, font=('Verdana 11'), bg=cor1, fg='#83a9e6')
+    l_sumario.place(x=309, y=115)
+    l_sumario = Label(frame_meio, text=f'R${valor[1]:,.2f}', anchor=NW, font=('Arial 17'), bg=cor1, fg='#545454')
+    l_sumario.place(x=309, y=150)
+
+    l_linha = Label(frame_meio, text='', width=220, height=1, anchor=NW, font=('Arial 1'), bg='#545454')
+    l_linha.place(x=309, y=207)
+    l_sumario = Label(frame_meio, text='Total de Saldo Mensal     '.upper(), anchor=NW, font=('Verdana 11'), bg=cor1, fg='#83a9e6')
+    l_sumario.place(x=309, y=190)
+    l_sumario = Label(frame_meio, text=f'R${valor[2]:,.2f}', anchor=NW, font=('Arial 17'), bg=cor1, fg='#545454')
+    l_sumario.place(x=309, y=220)
+
+# função gráfico pie
+def grafico_pie():
+    frame_gra_pie = Frame(frame_meio, width=580, height=250, bg=cor2)
+    frame_gra_pie.place(x=415, y=5)
+
+############################## frame gráfico ##############################
+frame_gra_pie = Frame(frame_meio, width=500, height=250, bg=cor2)
+frame_gra_pie.place(x=415, y=5)
+
+# função grafico pie
+def grafico_pie():
+    # faça figura e atribua objetos de eixo
+    figura = plt.Figure(figsize=(5, 3), dpi=90)
+    ax = figura.add_subplot(111)
+
+    lista_valores = [345,225,534]
+    lista_categorias = ['Renda', 'Despesa', 'Saldo']
+
+    # only "explode" the 2nd slice (i.e. 'Hogs')
+
+    explode = []
+    for i in lista_categorias:
+        explode.append(0.05)
+
+    ax.pie(lista_valores, explode=explode, wedgeprops=dict(width=0.2), autopct='%1.1f%%', colors=colors,shadow=True, startangle=90)
+    ax.legend(lista_categorias, loc="center right", bbox_to_anchor=(1.55, 0.50))
+
+    canva_categoria = FigureCanvasTkAgg(figura, frame_gra_pie)
+    canva_categoria.get_tk_widget().grid(row=0, column=0)
+
+
+
+
 
 porcentagem()
+grafico_barra()
+resumo()
+grafico_pie()
+
+
 janela.mainloop() 
