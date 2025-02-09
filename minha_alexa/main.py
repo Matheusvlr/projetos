b21c9e6c9e5918ca417d7ede26d2142ce58969e7
Matheusvlr <145072053+Matheusvlr@users.noreply.github.com>
2025-02-09 11:28:10 -0300

    main

---
+++ minha_alexa/main.py
@@ -0,0 +1,20 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+
+for voice in voices:
+    if 'portuguese' in voice.languages:
+        engine.setProperty('voice', voice.id)
+        break
+
+text = ' '
+
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate - 60)
+
+volume = engine.getProperty('volume')
+engine.setProperty('volume', volume + 0.25)
+
+engine.say(text)
+engine.runAndWait()
